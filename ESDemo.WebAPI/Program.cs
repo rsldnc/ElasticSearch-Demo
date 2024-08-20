80630ce91b0f7808aed9619ac736648a4f66e0b4
Resul Dinc <dincresull@gmail.com>
2024-08-20 04:09:54 +0300

    add methods

--- ESDemo.WebAPI/Program.cs
+++ ESDemo.WebAPI/Program.cs
@@ -1,4 +1,6 @@
+using Bogus;
 using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,108 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/products/create", async (CreateProductDto request, CancellationToken cancellationToken) =>
+{
+    Product product = new()
+    {
+        Name = request.Name,
+        Price = request.Price,
+        Stock = request.Stock,
+        Description = request.Description
+    };
+
+    CreateRequest<Product> createRequest = new(product.Id.ToString())
+    {
+        Document = product,
+    };
+
+    CreateResponse createResponse = await client.CreateAsync(createRequest, cancellationToken);
+
+    return Results.Ok(createResponse.Id);
+});
+
+app.MapPut("/products/update", async (UpdateProductDto request, CancellationToken cancellationToken) =>
+{
+
+    UpdateRequest<Product, UpdateProductDto> updateRequest = new("products", request.Id.ToString())
+    {
+        Doc = request
+    };
+
+    UpdateResponse<Product> updateResponse = await client.UpdateAsync(updateRequest, cancellationToken);
+
+    return Results.Ok(new { message = "Update is successful" });
+});
+
+app.MapDelete("/products/deleteById", async (Guid id, CancellationToken cancellationToken) =>
+{
+    DeleteResponse deleteResponse = await client.DeleteAsync("products", id, cancellationToken);
+
+    return Results.Ok(new { message = "Delete is successful" });
+});
+
+app.MapGet("/products/getall", async (CancellationToken cancellationToken) =>
+{
+    var searchRequest = new SearchRequest("products")
+    {
+        Size = 10000
+    };
+
+    SearchResponse<Product> response = await client.SearchAsync<Product>(searchRequest, cancellationToken);
+    return Results.Ok(response.Documents);
+});
+
+
+app.MapPost("/products/seeddata", async (CancellationToken cancellationToken) =>
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Faker faker = new();
+        Product product = new()
+        {
+            Name = faker.Commerce.ProductName(),
+            Price = Convert.ToDecimal(faker.Commerce.Price()),
+            Stock = faker.Commerce.Random.Int(1, 20),
+            Description = faker.Commerce.ProductDescription()
+        };
+
+        CreateRequest<Product> createRequest = new(product.Id.ToString())
+        {
+            Document = product
+        };
+        await client.CreateAsync(createRequest, cancellationToken);
+    }
+
+    return Results.Created();
+});
+
+
 
 app.Run();
+
+
+class Product
+{
+    public Product()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; } = default!;
+}
+
+record CreateProductDto(
+    string Name,
+    decimal Price,
+    int Stock,
+    string Description);
+
+record UpdateProductDto(
+    Guid Id,
+    string Name,
+    decimal Price,
+    int Stock,
+    string Description);
