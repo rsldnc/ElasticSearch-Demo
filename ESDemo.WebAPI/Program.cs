708d3a297c5588029fef90ff555784485f8dd9d9
Resul Dinc <dincresull@gmail.com>
2024-08-20 03:15:25 +0300

    configurate Elasticsearch

--- ESDemo.WebAPI/Program.cs
+++ ESDemo.WebAPI/Program.cs
@@ -1,7 +1,16 @@
+using Elastic.Clients.Elasticsearch;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
+
+ElasticsearchClientSettings settings = new(new Uri("http://localhost:9200"));
+settings.DefaultIndex("products");
+
+ElasticsearchClient client = new(settings);
+
+client.IndexAsync("products").GetAwaiter().GetResult();
 
 var app = builder.Build();
 
